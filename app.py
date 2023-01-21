513811898894131dca52369c8d69421931a6dd6d
Vethanathan <58129139+Vethanathan@users.noreply.github.com>
2023-01-21 10:34:33 +0530

    add signup page

--- app.py
+++ app.py
@@ -13,10 +13,18 @@ app.secret_key='a'
 def index():
     return render_template('home.html')
 
-@app.route('/success',methods=["POST"])
+@app.route('/dashboard',methods=["POST"])
 def final():
-    if request.method == 'POST':
-        print("hiii")
-    return "success"
+    user_name=request.form.get("email")
+    password=request.form.get("password")
+    print(user_name,password)
+    if user_name=="vethanathanvk@gmail.com" and password=="qwerty@12345":
+        return render_template("dashboard.html")
+    else:
+        return render_template("home.html")
+
+@app.route('/signup')
+def dashboard():
+    return render_template('signup.html')
 
 app.run(debug=1)
