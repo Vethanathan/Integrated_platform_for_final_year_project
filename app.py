c913621b08731424e9e4d65f2da13cc785f21d82
Vethanathan <58129139+Vethanathan@users.noreply.github.com>
2023-01-25 19:22:29 +0530

    deploy

--- app.py
+++ app.py
@@ -163,4 +163,4 @@ def vetha():
     #         f.write(html.replace("<!-- <p>vetha</p> -->", string))
     return render_template("dashboard.html")
 
-app.run(debug=1)
+app.run(debug=False,host='0.0.0.0')
